3ebc8afccbb608c00ce0c1c0e1f344079e8a93c3
franss22 <franortizz2001@gmail.com>
2023-07-24 21:10:50 -0400

    mas cosas finales

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-WINDOW_TIME_MS = 100 # Window time en s
-SEGMENTS_PER_SPEAKER = 5
+WINDOW_TIME_MS = 100 # Window time en ms
+SEGMENTS_PER_SPEAKER = 5 # Al generar un audio, cuantos samples de cada speaker hay en el audio final
 SEGLEN = 3
 FRAMERATE = 50
 DIMENSIONS = 32
